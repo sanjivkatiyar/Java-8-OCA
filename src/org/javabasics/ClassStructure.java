466ec53acbe3571bfff3f353c1acf22833b5917e
sanjiv <sanjiv.katiyar@gmail.com>
2022-06-18 14:12:25 -0700

    ok

---
+++ src/org/javabasics/ClassStructure.java
@@ -0,0 +1,21 @@
+package org.javabasics;
+
+public class ClassStructure {
+    // class and instance variables go here
+    static int count; // class variable
+    int x; // instance variable
+    static {} // static block
+    {} // instance block
+
+    ClassStructure(){} // constructor
+
+    public static void main(String[] args) {
+        // every program starts here
+    }
+
+    public static void m1() { // static method
+    }
+
+    public void m2 () { // instance method
+    }
+}
