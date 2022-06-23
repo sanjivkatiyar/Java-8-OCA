aaf72b163e5e707857b1f98fbe2353d6182c4a62
sanjiv <sanjiv.katiyar@gmail.com>
2022-06-22 23:31:03 -0700

    ok

--- src/org/javabasics/C23Method.java
+++ src/org/javabasics/C23Method.java
@@ -8,10 +8,22 @@ public class C23Method {
 
         sum = performCalc(5,7);
         System.out.println(sum);
+
+        double sumD = performCalc(5.0,7.0); //overloaded
+        System.out.println(sumD);
     }
 
     public static int performCalc(int x, int y){
         int sum = x+y;
         return sum;
     }
+
+    public static double performCalc(double x, double y){
+        double sum = x+y;
+        return sum;
+    }
+
+    // every method must have a return type or void.
+    // return; does not return anything but it's okay to use with void
+
 }
---
+++ src/org/javabasics/C25Constructor.java
@@ -0,0 +1,32 @@
+package org.javabasics;
+
+public class C25Constructor {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        Cat c = new Cat();
+        Mouse m = new Mouse();
+        Mouse m1 = new Mouse("Nibbles");
+    }
+}
+
+class Dog{
+    // will get a default constructor like below
+    /*Dog(){
+        super();
+    }*/
+}
+
+class Cat{
+    Cat(){}
+}
+
+class Mouse{
+    private String theName;
+    Mouse(){
+        theName="";
+    }
+    Mouse(String aName){
+        theName= aName;
+    }
+    void Mouse(){} // this is not a constructor but a badly written method
+}
