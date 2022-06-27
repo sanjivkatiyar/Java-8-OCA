2703dcfaea8e09cf4c8a77cbae7eee5bb4f10d9b
sanjiv <sanjiv.katiyar@gmail.com>
2022-06-26 17:20:39 -0700

    ok

---
+++ src/org/javabasics/C43StaticInterfaceMethod.java
@@ -0,0 +1,22 @@
+package org.javabasics;
+
+public class C43StaticInterfaceMethod {
+    public static void main(String[] args) {
+        System.out.println(StaticIface.m1());
+        new testSIF().go();
+    }
+}
+
+interface StaticIface{
+    static int m1(){return 4;} // public by default
+    public static void m2(){}
+    // static void m3(); // missing body
+
+}
+
+class testSIF implements StaticIface{
+    public void go(){
+        // System.out.println(m1());  // need interface_name.m1()
+        System.out.println(StaticIface.m1());
+    }
+}
