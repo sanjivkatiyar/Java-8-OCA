f7f957e65dc952ffc467c10f62456f44db41c824
sanjiv <sanjiv.katiyar@gmail.com>
2022-07-01 02:31:46 -0700

    ok

--- src/org/javabasics/C51TryCatch.java
+++ src/org/javabasics/C51TryCatch.java
@@ -1,6 +1,8 @@
 package org.javabasics;
 
 import java.io.EOFException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class C51TryCatch {
     public static void main(String[] args) {
@@ -46,5 +48,36 @@ public class C51TryCatch {
         // This method does not actually throw any exception at all.
         // Regardless, the compiler ensures however that caller has to catch
         // or throw EOFException
+    }
+    public static void inheritanceTree(){
+        try{
+            throw new FileNotFoundException();
+        } catch (FileNotFoundException e){}
+        catch (IOException e){}
+        catch (Exception e){}
+        // OK
+
+        try{
+            throw new FileNotFoundException();
+        } catch (IOException e){}
+        // catch (FileNotFoundException e){} // not okay as Exception already been caught by parent
+        catch (Exception e){}
+        // OK
+    }
+
+    public static void multiCatch(){
+        try{
+            // throw new FileNotFoundException(); // this alone is not okay at it
+            // will not reach EOFException and IOException
+            // If catch block were for RuntimeException only, then empty try block is ok.
+            throw new IOException(); // try block must throw IOException
+        } catch(FileNotFoundException|EOFException e){
+            // Identifier can appear only once, below will not compile.
+            // } catch(FileNotFoundException e|EOFException e){
+            // } catch(FileNotFoundException e1|EOFException e2){
+            // Exceptions must be siblings (no subclass relationship).
+            // below will fail to compile
+            // } catch(FileNotFoundException e1|IOException e2){
+        } catch(IOException e){}
     }
 }
