5c7925ac1a6e12341d1f530170ba9a7e7c6246ac
sanjiv <sanjiv.katiyar@gmail.com>
2022-07-02 00:55:48 -0700

    ok

--- src/org/javabasics/C59Arrays.java
+++ src/org/javabasics/C59Arrays.java
@@ -8,8 +8,33 @@ public class C59Arrays {
         c = new int[6];
         new C59Arrays().takesArray(new int[] {1,2,3});
 
+        ex1();
     }
-    void takesArray(int []a){
+    void takesArray(int []a){}
+    private static void ex1(){
+        Car1[] cars = {new Car1(), new Ferrari(), new Subaru()};
+    }
 
+    private static void ex2(){
+        Sporty[] car2 = {
+                new Subaru2(),    // ok
+                new Ferrari2(),   // ok, even comma is okay at last
+                //new Ford2()     // not okay
+        };
     }
 }
+
+class Car1{}
+class Subaru extends Car1{}
+class Ferrari extends  Car1{}
+
+
+interface Sporty{}
+class Car2{}
+class Subaru2 extends Car2 implements Sporty{}
+class Ferrari2 extends Car2 implements Sporty{}
+class Ford2 extends Car2{}
+
+
+
+
