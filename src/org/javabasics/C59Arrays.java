8c4984b67286fdfb173eb5513d05154197165153
sanjiv <sanjiv.katiyar@gmail.com>
2022-07-01 17:20:05 -0700

    ok

---
+++ src/org/javabasics/C59Arrays.java
@@ -0,0 +1,15 @@
+package org.javabasics;
+
+public class C59Arrays {
+    public static void main(String[] args) {
+        int[] a = new int[3];
+        int[] b = {1,2,3};
+        int c[];
+        c = new int[6];
+        new C59Arrays().takesArray(new int[] {1,2,3});
+
+    }
+    void takesArray(int []a){
+
+    }
+}
