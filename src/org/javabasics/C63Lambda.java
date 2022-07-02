bca61f7851c6b5634591ea55f2dfae7ca767db66
sanjiv <sanjiv.katiyar@gmail.com>
2022-07-02 13:38:46 -0700

    ok

--- src/org/javabasics/C63Lambda.java
+++ src/org/javabasics/C63Lambda.java
@@ -1,5 +1,7 @@
 package org.javabasics;
 
+import java.util.function.Predicate;
+
 public class C63Lambda {
 
     public static void main(String[] args) {
@@ -20,9 +22,36 @@ public class C63Lambda {
 
         inFa.m();
         inFa1.m();
+
+        ex1();
+    }
+
+    public static void ex1(){
+        Evaluate<Integer> lambda = i -> i<0;
+        System.out.println(lambda.isNegative(+1));
+        System.out.println(lambda.isNegative(-1));
+
+        // Predicate<T> is functional interface available in API has one abstract
+        // method boolean test(T t)
+        Predicate<Integer> predicate = i -> i<0;
+        // Predicate<Integer> predicate = i -> {return i<0;};   // same as above
+        System.out.println(predicate.test(+1));
+        System.out.println(predicate.test(-1));
+
+        System.out.println(check(4, i-> i>0));
+        System.out.println(check(4, i-> i<0));
+        System.out.println(check("Sanjiv", i-> i.startsWith("San")));
+    }
+
+    private static <T> boolean check(T t, Predicate<T> lambda){
+        return lambda.test(t);
     }
 }
 
 interface InFa{
     void m();      // a functional interface, only one method
+}
+
+interface Evaluate<T>{  // a functional interface, only one method
+    boolean isNegative(T t); // similar to predicate
 }
